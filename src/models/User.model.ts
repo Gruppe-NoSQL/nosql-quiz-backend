3b0e2ce1c485cc25dc0df12d11201dacbbb82d6f
stat1c <fynn@core-dev.de>
2020-10-13 17:36:44 +0200

    added score and deviceId

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -4,7 +4,8 @@ import IUser from './../interfaces/IUser';
 //User Schema
 const UserSchema: Schema = new Schema({
     username: {type: String, required: true},
-    
+    deviceId: {type: String, required: true},
+    score: {type: Number, default: 0}
 }, 
 {
     toObject: {
