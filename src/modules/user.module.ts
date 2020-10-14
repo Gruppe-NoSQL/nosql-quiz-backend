785b0b04821801cd5bd863a1379065de96f98c23
stat1c <fynn@core-dev.de>
2020-10-14 16:31:11 +0200

    reworked backend function to be type proof

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express';
 import UserModel from '../models/User.model';
+import IUser from '../interfaces/IUser';
 
 export default class HelloWorld {
 
@@ -22,41 +23,41 @@ export default class HelloWorld {
     private getUserList(req: Request, res: Response) {
         UserModel.find()
         .populate('question')
-        .then((users) => {
-            res.json(users)       
+        .then((users: IUser[]) => {
+            res.json(users);       
         })
         .catch(err => res.status(500).json(err));
     }
 
     private getUserById(req: Request, res: Response) {
-        UserModel.findById(req.params.id)
-        .then((user) => {
+        UserModel.findById(req.params.id, (err: any, user: IUser) => {
+            if(err || !user) { return err? res.status(500).json(err): res.status(400).json({'message': 'No User with an id of: ' + req.params.id}); }
+
             res.json(user);
-        })
-        .catch(err => res.status(500).json(err));
+        });
     }
 
     private createUser(req: Request, res: Response) {
-        UserModel.create(req.body)
-            .then((user) => {
-                res.json(user);
-            })
-            .catch(err => res.status(500).json(err));
+        UserModel.create(req.body, (err: any, user: IUser | any) => {
+            if(err) { return res.status(400).json(err); }
+            res.json(user);
+        });
     }
 
     private updateUser(req: Request, res: Response) {
-        UserModel.findByIdAndUpdate(req.params.id, req.body, { new: true})
-        .then((user) => {
+        UserModel.findByIdAndUpdate(req.params.id, req.body, { new: true}, (err: any, user: IUser | null) => {
+            if(err || !user) { return err? res.status(500).json(err): res.status(400).json({'message': 'No User with an id of: ' + req.params.id}); }
+
             res.json(user);
-        })
-        .catch(err => res.status(500).json(err));
+        });
     }
 
     private deleteUser(req: Request, res: Response) {
-        UserModel.findByIdAndDelete(req.params.id)
-        .then((user) => {
-            res.send("User " + req.params.id + " wurde erfolgreich gelöscht");
-        })
+        UserModel.findByIdAndDelete(req.params.id, (err: any, user: IUser | null) => {
+            if(err || !user) { return err? res.status(500).json(err): res.status(400).json({'message': 'No User with an id of: ' + req.params.id}); }
+
+            res.json(user);
+        });
     }
 
 
