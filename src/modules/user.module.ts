321221b62837a1e511f124a47a9ea581441633ea
lumaghg <rotkehlchen567@gmail.com>
2020-10-16 21:13:48 +0200

    added GET /user/:deviceId/

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -18,6 +18,7 @@ export default class HelloWorld {
         //setup Routes
         this.router.get('/', this.getUserList);
         this.router.get('/:deviceId', this.getUserById);
+        this.router.get('/:deviceId/isRegistered', this.isUserRegistered);
         this.router.post('/', this.createUser);
         this.router.put('/:id/sub', this.scoreUpdate);
         return this.router;
@@ -26,17 +27,25 @@ export default class HelloWorld {
     private getUserList(req: Request, res: Response) {
         UserModel.find()
             .exec((err: any, users: IUser[]) => {
-                if(err) {return res.status(500).json(err);}
+                if (err) { return res.status(500).json(err); }
                 res.json(users);
             });
     }
 
     private getUserById(req: Request, res: Response) {
-        
-        UserModel.findOne({deviceId : req.params.id}, (err: any, user: IUser) => {
+        UserModel.findOne({ deviceId: req.params.id }, (err: any, user: IUser) => {
             if (err || !user) { return err ? res.status(500).json(err) : res.status(400).json({ 'message': 'No User with an id of: ' + req.params.id }); }
             res.json(user);
         });
+    }
+
+    private isUserRegistered(req: Request, res: Response) {
+        UserModel.findOne({deviceId : req.params.deviceId}, (err: any, user: IUser) => {
+            if(user) {
+                return res.json({isRegistered: true});
+            } 
+            res.json({isRegistered: false});
+        })
     }
 
     private createUser(req: Request, res: Response) {
@@ -48,37 +57,29 @@ export default class HelloWorld {
 
     private scoreUpdate(req: Request, res: Response) {
         let score = 0;
-           req.body.forEach((userData: IQuestionSubSchema) => {
-                QuestionModel.findById(userData.questionId, (err: any, question: IQuestion) => {
-                    if(err) {return res.status(500).json(err)}
-                    if(userData.submission == question.correctAnswer) {
-                        userData.isAnswerCorrect = true;
-                        score++;
+        req.body.forEach((userData: IQuestionSubSchema) => {
+            QuestionModel.findById(userData.questionId, (err: any, question: IQuestion) => {
+                if (err) { return res.status(500).json(err) }
+                if (userData.submission == question.correctAnswer) {
+                    userData.isAnswerCorrect = true;
+                    score++;
+                }
+                UserModel.findOne({ deviceId: req.params.id }, (err: any, user: IUser) => {
+                    if (err) { res.status(400).json({ message: "kein User mit der id" + req.params.id }) }
+                    let userDataAcc = user.submissions;
+                    userDataAcc.push(userData);
+
+                    let userUpdate = {
+                        score: user.score + score,
+                        submissions: userDataAcc,
+                        questionId: userData.questionId
                     }
-                    UserModel.findOne({deviceId : req.params.id}, (err: any, user: IUser) => {
-                        if(err) {res.status(400).json({message: "kein User mit der id" + req.params.id})}
-                        let userDataAcc = user.submissions;
-                        userDataAcc.push(userData);
-
-                        let userUpdate = {
-                            score: user.score + score,
-                            submissions: userDataAcc,
-                            questionId: userData.questionId
-                        }
-                        UserModel.findOneAndUpdate({deviceId : req.params.id}, userUpdate, {new:true}, (err: any, user: any) => {
-                            if(err) {res.status(500).json(user);}
-                            res.json(user);
-                        })
-                        
+                    UserModel.findOneAndUpdate({ deviceId: req.params.id }, userUpdate, { new: true }, (err: any, user: any) => {
+                        if (err) { res.status(500).json(user); }
+                        res.json(user);
                     })
-                    
-                });
-
+                })
+            });
         })
-        
-        
     }
-    
-
-
 }
