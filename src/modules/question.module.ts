c0ae377821a48fde9af45d26fda164b9de7ea9db
lumaghg <rotkehlchen567@gmail.com>
2020-10-16 19:44:07 +0200

    added clearing of answers from the GET /question route

--- src/modules/question.module.ts
+++ src/modules/question.module.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 import QuestionModel from '../models/Question.model';
 import IQuestion from '../interfaces/IQuestion'
 import { Timestamp } from 'mongodb';
+import IQuestionSubSchema from '../interfaces/IQuestionSubSchema';
 
 
 export default class HelloWorld {
@@ -21,24 +22,30 @@ export default class HelloWorld {
     }
 
     private getQuestionList(req: Request, res: Response) {
-        QuestionModel.find()
-            .exec((err: any, questions: []) => {
-                if(err) {return res.status(500).json(err);}
-                res.json(questions);
-            });
-        }
+        QuestionModel.find((err: any, questions: Array<IQuestion>) => {
+            if (err) { return res.status(500).json(err); }
+            //remove solutions, unless it is for evaluation at the end
+            if (req.query.solutions == "true") {
+                return res.json(questions);
+            }
+            res.json(questions.map((question: IQuestion) => {
+                question.correctAnswer = "";
+                return question;
+            }));
+        });
+    }
 
     private getQuestionById(req: Request, res: Response) {
-       QuestionModel.findById(req.params.id, (err: any, question: IQuestion) => {
-        if (err || !question) { return err ? res.status(500).json(err) : res.status(400).json({ 'message': 'No Question with an id of: ' + req.params.id }); }
-        res.json(question);
-       });
-   
+        QuestionModel.findById(req.params.id, (err: any, question: IQuestion) => {
+            if (err || !question) { return err ? res.status(500).json(err) : res.status(400).json({ 'message': 'No Question with an id of: ' + req.params.id }); }
+            res.json(question);
+        });
+
     }
 
     private createQuestion(req: Request, res: Response) {
-        QuestionModel.create(req.body, (err: any, question: IQuestion | any) =>{
-            if(err){return res.status(400).json(err);}
+        QuestionModel.create(req.body, (err: any, question: IQuestion | any) => {
+            if (err) { return res.status(400).json(err); }
             res.json(question);
         });
     }
