b13f6705924f5dc32d81150e2f26733b44964eb2
lumaghg <robin.reyer@t-online.de>
2020-10-20 19:15:17 +0200

    security change on GET /question/:id

--- src/modules/question.module.ts
+++ src/modules/question.module.ts
@@ -3,6 +3,8 @@ import QuestionModel from '../models/Question.model';
 import IQuestion from '../interfaces/IQuestion'
 import { Timestamp } from 'mongodb';
 import IQuestionSubSchema from '../interfaces/IQuestionSubSchema';
+import UserModel from '../models/User.model';
+import IUser from '../interfaces/IUser';
 
 
 export default class HelloWorld {
@@ -15,8 +17,8 @@ export default class HelloWorld {
 
     public getRouter(): Router {
         //setup Routes
-        this.router.get('/', this.getQuestionList);
-        this.router.get('/:id', this.getQuestionById);
+        this.router.get('/:deviceId', this.getQuestionList);
+        this.router.get('/:deviceId', this.getQuestionById);
         this.router.post('/', this.createQuestion);
         return this.router;
     }
@@ -24,20 +26,24 @@ export default class HelloWorld {
     private getQuestionList(req: Request, res: Response) {
         QuestionModel.find((err: any, questions: Array<IQuestion>) => {
             if (err) { return res.status(500).json(err); }
-            //remove solutions, unless it is for evaluation at the end
-            if (req.query.solutions == "true") {
-                return res.json(questions);
+            UserModel.findOne({ deviceId: req.params.deviceId }, (err: any, user: IUser) => {
+                //remove solutions, unless user has alreay finished the quiz
+                if (!user) { res.status(400).json({ message: "Kein User mit der deviceId" + req.params.deviceId }) }
+                if (user.isFinished) {
+                    return res.json(questions);
+                }
+                res.json(questions.map((question: IQuestion) => {
+                    question.correctAnswer = "";
+                    return question;
+                }));
             }
-            res.json(questions.map((question: IQuestion) => {
-                question.correctAnswer = "";
-                return question;
-            }));
+            )
         });
     }
 
     private getQuestionById(req: Request, res: Response) {
-        QuestionModel.findById(req.params.id, (err: any, question: IQuestion) => {
-            if (err || !question) { return err ? res.status(500).json(err) : res.status(400).json({ 'message': 'No Question with an id of: ' + req.params.id }); }
+        QuestionModel.findById(req.params.deviceId, (err: any, question: IQuestion) => {
+            if (err || !question) { return err ? res.status(500).json(err) : res.status(400).json({ 'message': 'No Question with an id of: ' + req.params.deviceId }); }
             res.json(question);
         });
 
